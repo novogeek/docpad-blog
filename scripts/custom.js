81d2f2284fce2b90c3bbd58d407716d00be2c4b0
Krishna Chaitanya T <krishnachaitanya.t@live.com>
2013-11-25 02:38:52 +0530

    62139e5 fixed relative paths

---
+++ scripts/custom.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    if (!$('#myCanvas').tagcanvas({
+        textColour: '#2c3e50',
+        outlineColour: '#2c3e50',
+        initial: [0.1, -0.1],
+        reverse: true,
+        depth: 0.8,
+        maxSpeed: 0.05
+    }, 'divTagList')) {
+        // something went wrong, hide the canvas container
+        $('#divTagCanvas').hide();
+    }
+});
